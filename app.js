50ab50e5f9030c5a90dc3f4a6a315ee81976928b
mukhtar mohamed <170721974+donny-LD@users.noreply.github.com>
2024-06-07 10:07:58 +0100

    added keycodes

--- app.js
+++ app.js
@@ -4,27 +4,29 @@ window.addEventListener('keydown',(event) => {
     console.log(event.key);
     console.log(event.keyCode);
     console.log(event.code);
+    console.log(event.which);
 
     insert.innerHTML = `
-    <div class="key">
-       ${event.key === " " ? "space" : event.key} 
-      <small>event.key</small>
-    </div>
     <div class="key">
        ${event.keyCode} 
       <small>event.keycode</small>
     </div>
     <div class="key">
-       ${event.code} 
-      <small>event.code</small>
+       ${event.key === " " ? "space" : event.key} 
+      <small>event.key</small>
     </div>
     <div class="key">
-       ${event.location} 
-      <small>event.location</small>
+       ${event.code} 
+      <small>event.code</small>
     </div>
      <div class="key">
        ${event.which} 
       <small>event.which</small>
     </div>
+     <div class="key">
+       ${event.location} 
+      <small>event.location</small>
+    </div>
+
     `;
 });
